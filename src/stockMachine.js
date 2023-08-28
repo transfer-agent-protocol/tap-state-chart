351baeb6d5da0c8474dcbcb49efe0706e2e9f9e4
Victor Mimo <victormimoc@gmail.com>
2023-08-28 09:35:56 -0300

    Exploring new context and actions structure

--- src/stockMachine.js
+++ src/stockMachine.js
@@ -1,68 +1,118 @@
 import { createMachine } from "xstate";
+import { v4 as uuid } from "uuid";
 
-const stockIssuance = {
-  StockIssuance: {
-    target: "Issued",
-    actions: ["issue"],
-  },
-};
-
+// TODO: what should the "resting state" be?
 export const stockMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QGUAuB7AxgawAQFkBDTACwEsA7MAOgFUKzZYBXSAYjS2wEknnCKmMAG0ADAF1EoAA7pYZVGXQUpIAB6IAjADYATNQDsADk26ALAFYjJswE4AzAe0AaEAE9Eu7beqnd9o21Re00zO3sAXwjXThwCYnIqal4WdliePgEhMUkkEFl5RWVVDQRdA1FqR217Wu1tTRDtMNcPBEaDagsDMw6jUSsGo10omIw4olJKGhTWCA5x7ABBTCFpVCyRCVUChSUVPNKze21qPQaHAwtup00DVs9bfV7Q88077QsB0ZB0+KmkrM0osAMKbAA24MIRQoOR2cj2xUOiDMBmeBlsFlsBk0RhCFnsDwQZm0RmoRisJnsWPeJh+f0miRoKzWqGBXFmmzheV2MJKiCCnV0ol0gWF5jRmlsRKlZiqtlEtk+zWxFVsRnpi3+TOoLLA63ZOAAKgAnASwABmYBN3JkCL5yIQV3s5NFlgVunKxgsRN0T2oL16BiueICmq42umutW+rZ83SYMEYEh0P2tvy9v2-IQWLl9nKFL9+YcoqJJxdXgaohxFlRwZGPwo6AgcFUDIS03hhSzjoAtBZ9NSLLj-PnhTofe5EF9qNXeo4nLc7A2xhHGVH6IxUhAu4iDqBSr289cR7VPaIJ77-YHK2FTEYzOGJh3AXxILuHQfELVKkFLnjymaYIr2ed4zFvXo3SfPB1ySPUDR3HlMyRL8ED0TozAfd57BJUwKhcKcymvMCcTsK5rksaDIySRMhEhd8kO7FD1EQYNKmOYxGkxY4GgIto7E0LoKQXT4SWpSJol+LVYJoU1zStE0TQYu0mP3FiEGxfRzFCYN1VsEkHxAgMwIg+9HyiCIgA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QGUAuB7AxgawAQFkBDTACwEsA7MAOgFUKzZYBXSAYjS2wEknnCKmMAG0ADAF1EoAA7pYZVGXQUpIAB6IAjABYArNQCcAJk0AOAOyjTpnQYDM5gGwAaEAE9ERxweqPT200dtHWCjfwBfcNdOHAJicipqXhZ2GOwAQUwhaVQBITFJJBBZeUVlVQ0EbTtTam1RS3NzXW0AmsdzVw8ELx8-AKCQzTDtSOiMWKJSShpk1ggOCewAYTywABt1wjKKAtUShSUVIsrtcyM68wNdA3MzO01dOy7EbT9qU11rTTsbzTNNGMQGk4tNEplsqhUks5ms9kUDjsKohHA1qEZRGFHBijGcTAYXghNAZtNQ7AZRAZHLogrdLAZTECQVMEjQIWActCuAAVABOAlgADMwLz4TI5IdyidEM07B8RjdMUYmp9CUZjHV-sEml87DUmUtQazqOzOQs0qtBBstjsxcUJUjpQgbqS7MrPuq3fYwoS7I45V5HJoGo8zk0jJEoiAKOgIHBVMz4jN9g6jsiEABaXQXX66MxGPVGDGaamE4OOD4Uqk07R0ymMqOJsE0eiMFIQFOlNNOjOu3R50wFt3F0vuTwa4I6QOtYYRRuGlkzJJ8SCdyXHUCVPWiXyUhymN1OerPMc9Cda6ehOfjLhGpemqEdhGpqWblHKur+f52N7DSwuU91QuSczlsZp+z0A1b0XRJLSETZV2fLtX3UGVLDqfcfmuaogwA7pa00agvieJpqTeX47CgyYk0SPkBWFXleUQ8VkI3VCEFuC5cR0JoGRJfo1XPKdHCCK9RkjIA */
     id: "Stock Machine",
-
+    initial: "Unissued",
     context: {
-      stock: 0,
+      activePositions: {
+        // security_id:
+        "": {
+          stock_class_id: "",
+          quantity: 0,
+          share_price: {
+            currency: "",
+            amount: "",
+          },
+          timestamp: "",
+          accepted: false,
+        },
+      },
+      security_ids: [],
     },
-
     predictableActionArguments: true,
     preserveActionOrder: true,
-
     states: {
       Unissued: {
         on: {
-          ...stockIssuance,
+          StockIssuance: {
+            target: "Issued",
+            actions: ["issue"],
+          },
         },
       },
 
       Issued: {
         on: {
-          ...stockIssuance,
+          // not allowing more issuance until first position is accepted
           StockAcceptance: {
             target: "Accepted",
+            actions: ["accept"],
           },
           StockCancellation: {
             target: "Cancelled",
-            actions: ["remove"],
+            actions: ["cancel"],
           },
         },
       },
-
       Accepted: {
         on: {
-          ...stockIssuance,
+          StockIssuance: {
+            target: "Issued",
+            actions: ["issue"],
+          },
           StockTransfer: "Transferred",
           StockCancellation: {
             target: "Cancelled",
-            actions: ["remove"],
+            actions: ["cancel"],
           },
         },
       },
-
       Cancelled: {},
       Transferred: {},
     },
   },
   {
     actions: {
-      remove: (context, event) => {
-        context.stock = context.stock - event.value;
+      cancel: (context, event) => {
+        const activePosition = context.activePositions[event.value.security_id];
+        if (!activePosition) {
+          console.log("cannot find active position");
+          throw new Error("cannot find active position");
+        }
+        if (activePosition.quantity < event.value.quantity) {
+          console.log("cannot cancel more than issued");
+          throw new Error("cannot cancel more than issued");
+        }
+        // complete cancellation
+        if (activePosition.quantity === event.value.quantity) {
+          delete context.activePositions[event.value.security_id];
+
+          // partial cancellation
+        } else if (activePosition.quantity > event.value.quantity) {
+          const remainingQuantity =
+            activePosition.quantity - event.value.quantity;
+          console.log("remainingQuantity", remainingQuantity);
+          const newSecurityId = uuid();
+          context.activePositions[newSecurityId] = {
+            quantity: remainingQuantity,
+            timestamp: new Date().toISOString(),
+            ...activePosition,
+          };
+        }
       },
       issue: (context, event, obj) => {
         console.log("event ", event);
 
-        context.stock = context.stock + event.value;
+        context.activePositions[event.value.security_id] = {
+          stock_class_id: event.value.stock_class_id,
+          quantity: event.value.quantity,
+          share_price: event.value.share_price,
+          timestamp: new Date().toISOString(),
+          accepted: false,
+        };
+      },
+      accept: (context, event) => {
+        const activePosition = context.activePositions[event.value.security_id];
+        if (!activePosition) {
+          console.log("cannot find active position");
+          throw new Error("cannot find active position");
+        } else {
+          activePosition.accepted = true;
+        }
       },
     },
     services: {},
