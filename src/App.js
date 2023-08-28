a5cf6408389293679efdd4d460d94d29d26674cf
Victor Mimo <victormimoc@gmail.com>
2023-08-28 09:33:50 -0300

    Adding context object and stock acceptance to test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import { useMachine } from "@xstate/react";
 import { stockMachine } from "./stockMachine";
 import { useState } from "react";
+import { stockIssuanceData, stockAcceptanceData } from "./transactions";
 
 const Toggler = () => {
   const [state, send] = useMachine(stockMachine);
-  const [stock, setStock] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
   console.log("state", state);
   return (
     <div>
-      <div>Stock: {state.context.stock}</div>
+      <div>Context:</div>
+      <div>
+        <pre>{JSON.stringify(state.context, null, 4)}</pre>
+      </div>
+
       <div>Current State: {state.value}</div>
-      <input value={stock} onChange={(e) => setStock(+e.target.value)}></input>
-      <button onClick={() => send({ type: "StockIssuance", value: stock })}>
-        Issue
-      </button>
-      <button onClick={() => send({ type: "StockCancellation", value: stock })}>
-        Cancel
-      </button>
+      <div>
+        <input
+          value={quantity}
+          onChange={(e) => setQuantity(+e.target.value)}
+        ></input>
+        <button
+          onClick={() =>
+            send({
+              type: "StockIssuance",
+              value: { ...stockIssuanceData, quantity },
+            })
+          }
+        >
+          Issue
+        </button>
+        <button
+          onClick={() =>
+            send({ type: "StockAcceptance", value: stockAcceptanceData })
+          }
+        >
+          Accept
+        </button>
+        <button
+          onClick={() => send({ type: "StockCancellation", value: quantity })}
+        >
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };
